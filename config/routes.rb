c913a0c7e2a7c7f328a569bcfa31519108cc06bc
飯田恭子 <iidakyouko@mba.local>
2020-08-05 00:02:54 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+	resources :images
+  # get 'images/index'
+  # get 'images/new'
+  # get 'images/edit'
+  # get 'images/show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
